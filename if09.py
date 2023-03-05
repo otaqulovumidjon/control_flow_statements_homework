881a3491ed6cad36cdc3f5df9d09bf90d18d2e12
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-03-06 00:13:09 +0500

    if09

--- if09.py
+++ if09.py
@@ -9,4 +9,7 @@ def main(a):
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    return
+    if a==int(a) and a>=10 and a<=99:
+        return a >= (a%10)*10 + a//10
+
+print(main(32))
