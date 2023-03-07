f7ef410aa9f610367c625d30d17a03a956af12bf
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-03-07 10:25:59 +0500

    if05

--- if05.py
+++ if05.py
@@ -10,15 +10,15 @@ def main(a,b,c):
     """
     if a<0:
         a=1
-    else:
+    if a>0:
         a=0
     if b<0:
         b=1
-    else:
+    if b>0:
         b=0
     if c<0:
         c=1
-    else:
+    if c>0:
         c=0
     return a+b+c
 print(main(-2, -3, 4))
